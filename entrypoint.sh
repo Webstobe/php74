5312a56a44e0f5d1cd3d4ccd6c94bf633e5138b2
Nando Bosshart <nando@webstobe.ch>
2018-07-27 15:09:17 +0200

    - reset composer.lock and database on fresh install

--- entrypoint.sh
+++ entrypoint.sh
@@ -9,11 +9,15 @@ if  [ ! -f "./web/typo3conf/LocalConfiguration.php" ];
         echo -e "==================================="
         echo -e "== PREPARING INITIAL TYPO3-SETUP =="
         echo -e "==================================="
+        # reset existing database and composer.lock:
+        mysql -hmysql -u"$MYSQL_USER" -p"$MYSQL_PASSWORD"  <<< "DROP DATABASE IF EXISTS typo3; CREATE DATABASE typo3;"
+        rm -f composer.lock
         # first refresh the composer.lock file to have a proper install:
         composer update nothing --no-scripts
         #now run install-cmd
         composer install;
-        # restore DB:
+        # remove admin-user and restore default-DB:
+        mysql -hmysql -u"$MYSQL_USER" -p"$MYSQL_PASSWORD"  <<< "DELETE FROM typo3.be_users WHERE username='admin';"
         typo3cms database:import < /var/www/ingredients/mysql/initialdump.sql
     else
         echo -e "==================================="
