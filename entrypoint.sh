85cf18116891593baf4d6617de5275c50ba93169
Nando Bosshart <nando@webstobe.ch>
2018-07-30 11:58:36 +0200

    check if mySQL is ready

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,14 +1,27 @@
 #!/bin/bash
+RED="\033[0;31m"
+GREEN="\033[0;32m"
+ORANGE="\033[0;33m"
+NC="\033[0m"
 
 # change directory:
 cd /var/www
 
+# make sure mySQL-DB is ready:
+while [[ -z $(mysql -hmysql -u"$MYSQL_USER" -p"$MYSQL_PASSWORD"  <<< status)  ]]; do
+    echo -e "${RED}waiting for mySQl-Server...${NC}"
+    sleep 2
+done
+echo -e "${GREEN}->hello mySQl-Server :)${NC}"
+
+
 # only if /Localconfiguration.php is not already present:
 if  [ ! -f "./web/typo3conf/LocalConfiguration.php" ];
     then
-        echo -e "==================================="
-        echo -e "== PREPARING INITIAL TYPO3-SETUP =="
-        echo -e "==================================="
+        echo -e "${RED}==========================================${NC}"
+        echo -e "${RED}==     PREPARING INITIAL TYPO3-SETUP    ==${NC}"
+        echo -e "${RED}== existing database will be dropped !! ==${NC}"
+        echo -e "${RED}==========================================${NC}"
         # reset existing database and composer.lock:
         mysql -hmysql -u"$MYSQL_USER" -p"$MYSQL_PASSWORD"  <<< "DROP DATABASE IF EXISTS typo3; CREATE DATABASE typo3;"
         rm -f composer.lock
@@ -20,18 +33,18 @@ if  [ ! -f "./web/typo3conf/LocalConfiguration.php" ];
         mysql -hmysql -u"$MYSQL_USER" -p"$MYSQL_PASSWORD"  <<< "DELETE FROM typo3.be_users WHERE username='admin';"
         typo3cms database:import < /var/www/ingredients/mysql/initialdump.sql
     else
-        echo -e "==================================="
-        echo -e "== TYPO3 is already installed  =="
-        echo -e "==================================="
+        echo -e "${ORANGE}===================================${NC}"
+        echo -e "${ORANGE}== TYPO3 is already installed  ==${NC}"
+        echo -e "${ORANGE}===================================${NC}"
         composer update;
 fi
 
 # chown /var/www:
 chown -R www-data:www-data /var/www
 
-echo -e "==================================="
-echo -e "==      CONTAINER IS READY       =="
-echo -e "==================================="
+echo -e "${GREEN}===================================${NC}"
+echo -e "${GREEN}==      CONTAINER IS READY       ==${NC}"
+echo -e "${GREEN}===================================${NC}"
 
 # show PHP-Version:
 php -v
